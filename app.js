a71b45b7ed22d9d771187c02cc774b28d4a41dbd
vitorvitorino <vitoroollii@gmail.com>
2023-05-28 19:21:55 -0300

    Cruzando os dedos vendo se o restore vai funcionar

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log('Criando o sistema de gerenciamento de jogos');
 console.log('Testando as capacidades do git');
 console.log('Modificando local e vendo se a mudança é refletida no github')
-console.log('Essa modificação será apagada pois eu irei testar o git restore');
