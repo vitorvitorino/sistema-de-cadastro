92c50ba6324f0d8d0db664299d84b8baa6ad2c60
vitorvitorino <vitoroollii@gmail.com>
2023-05-28 19:46:49 -0300

    Testando o git add e torcendo para que tudo funcione

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log('Criando o sistema de gerenciamento de jogos');
 console.log('Testando as capacidades do git');
 console.log('Modificando local e vendo se a mudança é refletida no github')
+console.log('Essa mudança é só para testar o git add');
