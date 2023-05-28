6d7157015307f8e1edb8d6b85534f4359d1231f2
vitorvitorino <vitoroollii@gmail.com>
2023-05-28 19:20:00 -0300

    commitando uma mudança para testar o git restore

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log('Criando o sistema de gerenciamento de jogos');
 console.log('Testando as capacidades do git');
 console.log('Modificando local e vendo se a mudança é refletida no github')
+console.log('Essa modificação será apagada pois eu irei testar o git restore');
