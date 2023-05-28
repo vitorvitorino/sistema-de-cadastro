26d4d2e276de522585c56312ed9c922bf03ce829
vitorvitorino <vitoroollii@gmail.com>
2023-05-28 19:12:34 -0300

    Fazendo outra mudança local e vendo se o commit e o push funcionam

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log('Criando o sistema de gerenciamento de jogos');
 console.log('Testando as capacidades do git');
+console.log('Modificando local e vendo se a mudança é refletida no github')
